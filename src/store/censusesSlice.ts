b12e057fd425e9c5997a02a6bf429720c32a3ae8
polthm <pol.thomas56@gmail.com>
2023-02-26 19:48:42 +0100

    remove updateCensus

--- src/store/censusesSlice.ts
+++ src/store/censusesSlice.ts
@@ -27,12 +27,6 @@ export const censusesSlice = createSlice({
       state.censuses = [...state.censuses, action.payload];
     },
 
-    updateCensus: (state, action: PayloadAction<Census>) => {
-      if (action.payload.id === null) return;
-      const index = findCensusIndex(state, action.payload.id);
-      state.censuses[index] = action.payload;
-    },
-
     deleteCensus: (state, action: PayloadAction<number>) => {
       const index = findCensusIndex(state, action.payload);
       state.censuses.splice(index, 1);
@@ -40,7 +34,7 @@ export const censusesSlice = createSlice({
   },
 });
 
-export const { addCensus, updateCensus, deleteCensus, fetchCensuses } =
+export const { addCensus, deleteCensus, fetchCensuses } =
   censusesSlice.actions;
 
 export default censusesSlice.reducer;
