d9aa25eaaeba7dcaa9bc1f0dad47f787b2501d15
polthm <pol.thomas56@gmail.com>
2023-02-28 04:16:41 +0100

    Stats page done

---
+++ src/components/StatCard.tsx
@@ -0,0 +1,60 @@
+import { Card, CardContent, Typography } from '@mui/material';
+import React, { FC } from 'react';
+
+type Props = {
+  title: string;
+  score: number;
+  scoreVariant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+};
+
+const StatCard: FC<Props> = ({ title, score, scoreVariant }) => {
+  return (
+    <Card
+      sx={{
+        backgroundColor: 'transparent',
+        width: 200,
+        height: 200,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'start',
+        mx: 'auto',
+        pt: 2,
+      }}
+      variant="elevation"
+    >
+      <CardContent
+        sx={{
+          color: 'text.primary',
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          height: 1,
+        }}
+      >
+        <Typography variant="h5" component="p" textAlign="center" gutterBottom>
+          {title}
+        </Typography>
+        <Typography
+          variant={scoreVariant}
+          component="p"
+          textAlign="center"
+          color="primary"
+          sx={{
+            display: 'flex',
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {score}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+StatCard.defaultProps = {
+  scoreVariant: 'h1',
+};
+
+export default StatCard;
--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -1,26 +1,57 @@
-import { Container, Typography } from '@mui/material';
-import React, { FC } from 'react';
+import { Container, Grid, Typography } from '@mui/material';
+import { FC, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
+import StatCard from '@/components/StatCard';
 import { RootState } from '@/store';
 
 const StatisticsPage: FC = () => {
   const censuses = useSelector(
     (state: RootState) => state.censusesStore.censuses
   );
+  const [ageAverage, setAgeAverage] = useState(0);
+  const [incomeAverage, setIncomeAverage] = useState(0);
+  const [residentsAverage, setResidentsAverage] = useState(0);
 
-  const getAgeAverage = () => {
-    const filteredCensuses = censuses.filter(
-      (census) => census.profile.age !== undefined
-    );
-
-    return (
-      filteredCensuses.reduce((acc, census) => {
+  const setAge = () => {
+    const age =
+      censuses.reduce((acc, census) => {
         if (!census.profile.age) return acc;
         return acc + census.profile.age;
-      }, 0) / filteredCensuses.length
-    );
+      }, 0) / censuses.length;
+
+    setAgeAverage(Math.floor(age));
   };
+
+  const setIncome = () => {
+    const income =
+      censuses.reduce((acc, census) => {
+        if (!census.profile.income) return acc;
+        return acc + census.profile.income;
+      }, 0) / censuses.length;
+
+    setIncomeAverage(Math.floor(income));
+  };
+
+  const setResidents = () => {
+    const residents =
+      censuses.reduce((acc, census) => {
+        if (!census.lodging.residents) return acc;
+        return acc + census.lodging.residents;
+      }, 0) / censuses.length;
+
+    setResidentsAverage(Math.floor(residents));
+  };
+
+  const setAllStats = () => {
+    setAge();
+    setIncome();
+    setResidents();
+  };
+
+  useEffect(() => {
+    setAllStats();
+  });
 
   return (
     <Container
@@ -37,16 +68,24 @@ const StatisticsPage: FC = () => {
         Statistics Globales
       </Typography>
       <Typography variant="h4" textAlign="center">
-        Voici une page ou vous pourrez retrouver les statistiques globales
+        Voici quelques moyennes de nos recentes enquêtes
       </Typography>
 
-      <Typography
-        variant="h4"
-        textAlign="center"
-        sx={{ mt: 4, color: 'primary.main' }}
-      >
-        Age moyen : {getAgeAverage()} ans
-      </Typography>
+      <Grid container spacing={4} sx={{ my: 4 }}>
+        <Grid item xs={12} sm={6} md={4}>
+          <StatCard title="Âge" score={ageAverage} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <StatCard
+            title="Revenu annuel"
+            score={incomeAverage}
+            scoreVariant="h3"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <StatCard title="Résidents" score={residentsAverage} />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
