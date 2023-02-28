0ca4df6d140c0c6de296384a2334ceee5d4505a9
polthm <pol.thomas56@gmail.com>
2023-02-28 14:22:03 +0100

    feat: add back-button ut

---
+++ src/__tests__/BackButton.test.tsx
@@ -0,0 +1,13 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, it, vitest } from 'vitest';
+
+import BackButton from '@/components/shared/BackButton';
+
+describe('BackButton', () => {
+  it('should call onClick function when button is clicked', () => {
+    const onClickMock = vitest.fn();
+    render(<BackButton onClick={onClickMock} />);
+    fireEvent.click(screen.getByText('Retour'));
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
