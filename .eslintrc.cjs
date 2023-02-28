685004df3f82328da9da7b81a86910a90acd031a
polthm <pol.thomas56@gmail.com>
2023-02-28 19:31:05 +0100

    Fix lint issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -53,10 +53,10 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
-        'argsIgnorePattern': '^_',
-        'varsIgnorePattern': '^_',
-        'caughtErrorsIgnorePattern': '^_'
-      }
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
     ],
   },
 };
