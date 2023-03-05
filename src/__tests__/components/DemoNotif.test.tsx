00a1562ae801a19dcd9deaa7c43c5ad4a9addecc
Pol Thomas <pol.thomas56@gmail.com>
2023-03-05 16:32:57 +0100

    feat: add a demo-notif unit tests with fake timers (#23)
    * feat: add a demo-notif unit tests with fake timers
    
    * small refacto

---
+++ src/__tests__/components/DemoNotif.test.tsx
@@ -0,0 +1,29 @@
+import { act, fireEvent, render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+
+import DemoNotif from '@/components/DemoNotif';
+
+describe('DemoNotif', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('should render the correct message after 1s', () => {
+    render(<DemoNotif />);
+    expect(screen.queryByText('Ceci est une démo')).not.toBeInTheDocument();
+    act(() => vi.advanceTimersByTime(1000));
+    expect(screen.getByText('Ceci est une démo')).toBeInTheDocument();
+  });
+
+  it('should close the notification when clicking on the close button (once it is open) after a small transition', () => {
+    render(<DemoNotif />);
+    act(() => vi.advanceTimersByTime(1000));
+    fireEvent.click(screen.getByTestId('CloseIcon'));
+    act(() => vi.advanceTimersByTime(500));
+    expect(screen.queryByText('Ceci est une démo')).not.toBeInTheDocument();
+  });
+});
