b5ecdcd861f67a7bb7dd44b0e907123cdbb69cbb
polthm <pol.thomas56@gmail.com>
2023-03-05 15:19:06 +0100

    feat: add redux config for ut and statistics page ut

--- src/__tests__/pages/StatisticsPage.test.tsx
+++ src/__tests__/pages/StatisticsPage.test.tsx
@@ -1,5 +1,4 @@
-import { fireEvent, screen } from '@testing-library/react';
-import { vi } from 'vitest';
+import { screen } from '@testing-library/react';
 
 import StatisticsPage from '@/pages/StatisticsPage';
 import { setupStore } from '@/store';
@@ -11,5 +10,36 @@ describe('StatisticsPage', () => {
   it('should render the title', () => {
     renderWithProviders(<StatisticsPage />, { store });
     expect(screen.getByText('Statistics Globales')).toBeInTheDocument();
+  });
+
+  it('should render the subtitle', () => {
+    renderWithProviders(<StatisticsPage />, { store });
+    expect(
+      screen.getByText('Voici quelques chiffres tirés de nos recensements')
+    ).toBeInTheDocument();
+  });
+
+  it('should render the description', () => {
+    renderWithProviders(<StatisticsPage />, { store });
+    expect(
+      screen.getByText(
+        'Chaque résultat représente la moyenne des données recueillies (arrondies)'
+      )
+    ).toBeInTheDocument();
+  });
+
+  it('should render the age average card', () => {
+    renderWithProviders(<StatisticsPage />, { store });
+    expect(screen.getByText('Âge')).toBeInTheDocument();
+  });
+
+  it('should render the income average card', () => {
+    renderWithProviders(<StatisticsPage />, { store });
+    expect(screen.getByText('Revenu annuel')).toBeInTheDocument();
+  });
+
+  it('should render the residents average card', () => {
+    renderWithProviders(<StatisticsPage />, { store });
+    expect(screen.getByText('Résidents')).toBeInTheDocument();
   });
 });
