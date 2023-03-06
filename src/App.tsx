7e6dfd6bd2d2075117b73c677762ecca1080eda7
polthm <pol.thomas56@gmail.com>
2023-03-06 23:37:54 +0100

    refactor: extract router-content from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { Box, Paper } from '@mui/material';
-import { AnimatePresence, motion } from 'framer-motion';
+import { Box } from '@mui/material';
 import { FC, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Route, Routes, useLocation } from 'react-router-dom';
 
 import DemoNotif from '@/components/DemoNotif';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
-import NotFoundPage from '@/pages/NotFoundPage';
-import appRoutes from '@/routes';
+import RouterContent from '@/components/RouterContent';
 import { setAllCensuses } from '@/store/censusesSlice';
 import { getFirebaseDbAndSetAllCensuses } from '@/utils/firebase-utils';
 import { getCensusesFromLocalStorage } from '@/utils/local-storage-utils';
 
 const App: FC = () => {
   const dispatch = useDispatch();
-  const location = useLocation();
 
   useEffect(() => {
     const localCensusesDb = getCensusesFromLocalStorage();
@@ -34,33 +30,7 @@ const App: FC = () => {
       sx={{ height: '100dvh' }}
     >
       <Navbar />
-      <Paper
-        elevation={3}
-        sx={{ p: 2, flex: 1 }}
-        style={{ animation: 'fadein 2s ease' }}
-      >
-        <AnimatePresence mode="wait" initial={false}>
-          <motion.main
-            key={location.pathname}
-            style={{ height: '100%' }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.15 }}
-          >
-            <Routes location={location} key={location.pathname}>
-              {appRoutes.map((route) => (
-                <Route
-                  key={route.key}
-                  path={route.path}
-                  element={<route.component />}
-                />
-              ))}
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </motion.main>
-        </AnimatePresence>
-      </Paper>
+      <RouterContent />
       <Footer />
       <DemoNotif />
     </Box>
