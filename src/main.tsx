c8be4b1e5779d42612b899f60de02cfc6e96d172
polthm <pol.thomas56@gmail.com>
2023-03-06 15:07:55 +0100

    try to use dotenv

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import '@/assets/style/main.scss';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import * as dotenv from 'dotenv';
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -9,6 +10,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import theme from '@/assets/style/theme';
 import { setupStore } from '@/store';
+
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
