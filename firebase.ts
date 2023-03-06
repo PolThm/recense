aa8e679c1078abe6ef4efb6acf53f338d7c4a8ee
polthm <pol.thomas56@gmail.com>
2023-03-06 14:38:28 +0100

    Revert "feat: add env file (#28)"
    This reverts commit c10a95083c6425c39d3714c14e41e521191f13f7.

--- firebase.ts
+++ firebase.ts
@@ -1,15 +1,18 @@
+// This database can only be read from, not written to.
+
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_DATABASE_URL,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyA68IHFCQ0ysps9M9wJVjsxf13G0a2IYkQ',
+  authDomain: 'recense-7dd15.firebaseapp.com',
+  databaseURL:
+    'https://recense-7dd15-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'recense-7dd15',
+  storageBucket: 'recense-7dd15.appspot.com',
+  messagingSenderId: '20554665614',
+  appId: '1:20554665614:web:61bb7af219aacc290f43df',
+  measurementId: 'G-9RM0PFPRLN',
 };
 
 const app = initializeApp(firebaseConfig);
