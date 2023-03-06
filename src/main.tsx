b777669f8adab97a8e518ed29136156ecf8dfc26
polthm <pol.thomas56@gmail.com>
2023-03-06 15:13:47 +0100

    remove dotenv

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import '@/assets/style/main.scss';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import * as dotenv from 'dotenv';
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -10,8 +9,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import theme from '@/assets/style/theme';
 import { setupStore } from '@/store';
-
-dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
