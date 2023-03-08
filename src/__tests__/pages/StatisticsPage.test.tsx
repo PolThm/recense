b7989106fae980c2e034d99e43a78654b6f09ecb
polthm <pol.thomas56@gmail.com>
2023-03-08 14:23:06 +0100

    fix statistics page unit tests

--- src/__tests__/pages/StatisticsPage.test.tsx
+++ src/__tests__/pages/StatisticsPage.test.tsx
@@ -1,11 +1,11 @@
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { beforeEach } from 'vitest';
 
 import CensusMock from '@/__tests__/mocks/CensusMock';
 import StatisticsPage from '@/pages/StatisticsPage';
 import { setupStore } from '@/store';
-import { setAllCensuses, setIsLoading } from '@/store/censusesSlice';
+import { setAllCensuses } from '@/store/censusesSlice';
 import { renderWithProviders } from '@/utils/tests-utils';
 
 describe('StatisticsPage', () => {
@@ -38,27 +38,30 @@ describe('StatisticsPage', () => {
   });
 
   it('should render the CircularProgress component if it is loading', () => {
-    store.dispatch(setIsLoading(true));
     renderWithProviders(<StatisticsPage />, { store });
     expect(screen.getByRole('progressbar')).toBeInTheDocument();
   });
 
-  it('should render the age average card if it is not loading and there are censuses', () => {
+  it('should render the age average card if it is not loading and there are censuses', async () => {
     renderWithProviders(<StatisticsPage />, { store });
-    expect(screen.getByText('Âge')).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText('Âge')).toBeInTheDocument());
   });
 
-  it('should render the income average card if it is not loading and there are censuses', () => {
+  it('should render the income average card if it is not loading and there are censuses', async () => {
     renderWithProviders(<StatisticsPage />, { store });
-    expect(screen.getByText('Revenu annuel')).toBeInTheDocument();
+    await waitFor(() =>
+      expect(screen.getByText('Revenu annuel')).toBeInTheDocument()
+    );
   });
 
-  it('should render the residents average card if it is not loading and there are censuses', () => {
+  it('should render the residents average card if it is not loading and there are censuses', async () => {
     renderWithProviders(<StatisticsPage />, { store });
-    expect(screen.getByText('Résidents')).toBeInTheDocument();
+    await waitFor(() =>
+      expect(screen.getByText('Résidents')).toBeInTheDocument()
+    );
   });
 
-  it('should render the empty censuses warning if it is not loading and there are no censuses', () => {
+  it('should render the empty censuses warning if it is not loading and there are no censuses', async () => {
     store.dispatch(setAllCensuses([]));
     renderWithProviders(
       <MemoryRouter>
@@ -66,6 +69,8 @@ describe('StatisticsPage', () => {
       </MemoryRouter>,
       { store }
     );
-    expect(screen.getByTestId('empty-censuses-warning')).toBeInTheDocument();
+    await waitFor(() =>
+      expect(screen.getByTestId('empty-censuses-warning')).toBeInTheDocument()
+    );
   });
 });
