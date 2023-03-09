99ea8377f9401e96a59c4f14bc10eac9df84159a
polthm <pol.thomas56@gmail.com>
2023-03-09 12:55:09 +0100

    feat: add my-checkbox unit tests

---
+++ src/__tests__/components/shared/MyCheckbox.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+
+import MyCheckbox from '@/components/shared/MyCheckbox';
+
+vi.mock('formik', () => ({
+  useField: () => [
+    {
+      value: '',
+      onChange: vi.fn(),
+    },
+    { touched: false, error: '' },
+  ],
+}));
+
+describe('MyCheckbox', () => {
+  it('should render a checkbox (with label)', () => {
+    render(<MyCheckbox name="accept">Accepter les conditions</MyCheckbox>);
+    expect(
+      screen.getByLabelText('Accepter les conditions')
+    ).toBeInTheDocument();
+    expect(screen.getByRole('checkbox')).toBeInTheDocument();
+  });
+});
